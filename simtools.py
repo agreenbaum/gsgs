980619cd71b86f80d592c5d6f4fec9d0fe047054
Alexandra Greenbaum <agreenba@pha.jhu.edu>
2016-06-23 07:13:32 -0400

    updated basic demo, still needs mask geom script.

--- simtools.py
+++ simtools.py
@@ -2,8 +2,15 @@
 
 import numpy as np
 import sys,os
-import zerniketools as zern
 
+
+def mas2rad(mas):
+	rad = mas*(10**(-3)) / (3600*180/np.pi)
+	return rad
+
+def rad2mas(rad):
+	mas = rad * (3600*180/np.pi) * 10**3
+	return mas
 
 def makedisk(N, R, ctr=(0,0)):
 	if N%2 == 1:
@@ -20,70 +27,221 @@ def makedisk(N, R, ctr=(0,0)):
 	array[r<R] = 1
 	return array
 
+def makeA(nh, verbose=False):
+	""" 
+	Writes the "NRM matrix" that gets pseudo-inverterd to provide
+	(arbitrarily constrained) zero-mean phases of the holes.
 
-def simulate_zern(fov = 80, mode=5, livepupilrad=None, mask=None):
-	"""
-	Default Astig_1 0.1 radians
-	"""
-	# Define our coordinates for wavefront aberration of choice
-	y,x = np.indices((fov,fov)) - fov/2
-	if livepupilrad is not None:
-		rho = np.sqrt( (x*x) + (y*y) ) / livepupilrad
-	else:
-		rho = np.sqrt( (x*x) + (y*y) ) / (fov/2) 
-	theta = np.arctan2(y,x)
-	if hasattr(mode, "__iter__"):
-		aberr = np.zeros(rho.shape)
-		frac = 1/len(mode)
-		for md in mode:
-			aberr += zern.zernikel(md,rho,theta)
-	else:
-		aberr = zern.zernikel(mode, rho, theta)
-	if mask is not None:
-		mask = mask
-	else:
-		mask = np.ones((fov,fov))
-	mask_aberr = mask * aberr
-	# print out rms of this aberration over circular pupil?  - AS
-	print np.var(aberr[rho<1])
-	return mask_aberr, aberr
+	makeA taken verbatim from Anand's pseudoinverse.py
 
-def avg_piston(aberr, positions, mask, R = 20, point=False):
-	"""
-	aberr is the full square array aberration
+	 input: nh - number of holes in NR mask
+	 input: verbose - True or False
+	 output: A matrix, nh columns, nh(nh-1)/2 rows  (eg 21 for nh=7)
 
-	positions are approximaete coordinates of mask holes recorded by eye
+	Ax = b  where x are the nh hole phases, b the nh(nh-1)/2 fringe phases,
+	and A the NRM matrix
 
-	mask is the NRM or other pupil mask for over which to compute average phases
+	Solve for the hole phases:
+		Apinv = np.linalg.pinv(A)
+		Solution for unknown x's:
+		x = np.dot(Apinv, b)
 
-	R - radius to enclose mask holes around coordinate points (also by eye for now)
-	    purpose to choose holes one by one. 
+	Following Noah Gamper's convention of fringe phases,
+	for holes 'a b c d e f g', rows of A are 
 
-	point -- should this return average phase values in a single pixel (TRUE)
-		 or over the whole mask support (FALSE). Recommended to remain FALSE.
-	"""
-	fov = aberr.shape[0]
-	avgphase = np.zeros(aberr.shape) #radians
-	holemask = avgphase.copy()
-	x,y = np.indices(holemask.shape)
-	debug = avgphase.copy()
-	new_support = avgphase.copy()
-	for ii,pos in enumerate(positions):
-		holemask = makedisk(fov, R, ctr = (pos[0] - fov/2, pos[1] - fov/2))
-		debug+=holemask
-		holemask[abs(holemask*mask)>0] = 1 
-		holemask[abs(holemask*mask)==0] = 0
-		if not point:
-			avgphase[holemask==1] = aberr[holemask==1].sum() / len(aberr[holemask==1])
-		else:
-			#int(np.sum(holemask*x)/holemask.sum()),int(np.sum(holemask*y)/holemask.sum())
-			avgphase[int(np.sum(holemask*x)/holemask.sum()),\
-			 int(np.sum(holemask*y)/holemask.sum())] = \
-					aberr[holemask==1].sum() / len(aberr[holemask==1])
-		#plt.figure()
-		#plt.imshow(avgphase)
-		#plt.figure()
-		#plt.imshow(debug)
-		#plt.show()
-	new_support[abs(avgphase)>0] = 1
-	return avgphase, new_support
+	    (-1 +1  0  0  ...)
+	    ( 0 -1 +1  0  ...)
+	
+
+	which is implemented in makeA() as:
+		matrixA[row,h2] = -1
+		matrixA[row,h1] = +1
+
+	To change the convention just reverse the signs of the 'ones'.
+
+	When tested against Alex'' NRM_Model.py "piston_phase" text output of fringe phases, 
+	these signs appear to be correct - anand@stsci.edu 12 Nov 2014
+
+	anand@stsci.edu  29 Aug 2014
+		"""
+
+	print "\nmakeA(): "
+	#                   rows         cols
+	ncols = (nh*(nh-1))//2
+	nrows = nh
+	matrixA = np.zeros((ncols, nrows))
+	if verbose: print matrixA
+	row = 0
+	for h2 in range(nh):
+		if verbose: print
+		for h1 in range(h2+1,nh):
+			if h1 >= nh:
+				break
+			else:
+				if verbose:
+					print "R%2d: "%row, 
+					print "%d-%d"%(h1,h2)
+				matrixA[row,h2] = -1
+				matrixA[row,h1] = +1
+				row += 1
+	if verbose: print
+	return matrixA
+
+
+def makeK(nh, verbose=False):
+	""" 
+	As above, write the "kernel matrix" that converts fringe phases
+	to closure phases. This can be psuedo-inverted to provide a 
+	subset of "calibrated" fringe phases (hole-based noise removed)
+
+	 input: nh - number of holes in NR mask
+	 input: verbose - True or False
+	 output: L matrix, nh(nh-1)/2 columns, comb(nh, 3) rows  (eg 35 for nh=7)
+
+	Kx = b, where: 
+		- x are the nh(nh-1)/2 calibrated fringe phases 
+		- b the comb(nh, 3) closure phases,
+	and K the kernel matrix
+
+	Solve for the "calibrated" phases:
+		Kpinv = np.linalg.pinv(K)
+		Solution for unknown x's:
+		x = np.dot(Kpinv, b)
+
+	Following the convention of fringe phase ordering above, which should look like:
+	h12, h13, h14, ..., h23, h24, ....
+	rows of K should look like:
+
+	    (+1 -1  0  0  0  0  0  0 +1 ...) e.g., h12 - h13 + h23
+	    (+1 +1  0 +1  ...)
+	
+
+	which is implemented in makeK() as:
+		matrixK[n_cp, f12] = +1
+		matrixK[n_cp, f13] = -1
+		matrixK[n_cp, f23] = +1
+
+	need to define the row selectors
+	 k is a list that looks like [9, 9+8, 9+8+7, 9+8+7+6, 9+8+7+6+5, ...] 
+	 -----up to nh*(nh-1)/2
+	 i is a list that looks like [0,9, 9+8, 9+8+7, 9+8+7+6, 9+8+7+6+5, ...] 
+	 -----up to nh*(nh-1)/2 -1
+	because there are 9 fringe phases per single hole (decreasing by one to avoid repeating)
+	hope that helps explain this!
+
+	agreenba@pha.jhu.edu  22 Aug 2015
+		"""
+
+	print "\nmakeK(): "
+	nrow = comb(nh, 3)
+	ncol = nh*(nh-1)/2
+
+	# first define the row selectors
+	# k is a list that looks like [9, 9+8, 9+8+7, 9+8+7+6, 9+8+7+6+5, ...] 
+	# -----up to nh*(nh-1)/2
+	# i is a list that looks like [0,9, 9+8, 9+8+7, 9+8+7+6, 9+8+7+6+5, ...] 
+	# -----up to nh*(nh-1)/2 -1
+	countk=[]
+	val=0
+	for q in range(nh-1):
+		val = val + (nh-1)-q
+		countk.append(val)
+	counti = [0,]+countk[:-1]
+	# MatrixK
+	row=0
+	matrixK = np.zeros((nrow, ncol))
+	for ii in range(nh-2):
+		for jj in range(nh-ii-2):
+			for kk in range(nh-ii-jj-2):
+				matrixK[row+kk, counti[ii]+jj] = 1
+				matrixK[row+kk, countk[ii+jj]+kk] = 1
+				matrixK[row+kk, counti[ii]+jj+kk+1] = -1
+			row=row+kk+1
+	if verbose: print
+
+	return matrixK
+
+def baselinify(ctrs):
+	N = len(ctrs)
+	uvs = np.zeros((N*(N-1)//2, 2))
+	label = np.zeros((N*(N-1)//2, 2))
+	bllengths = np.zeros(N*(N-1)//2)
+	nn=0
+	for ii in range(N-1):
+		for jj in range(N-ii-1):
+			uvs[jj+nn, 0] = ctrs[ii,0] - ctrs[ii+jj+1,0]
+			uvs[jj+nn, 1] = ctrs[ii,1] - ctrs[ii+jj+1,1]
+			bllengths[jj+nn] = np.sqrt((ctrs[ii,0]-ctrs[ii+jj+1,0])**2 +\
+					    (ctrs[ii,1]-ctrs[ii+jj+1,1])**2)
+			label[jj+nn,:] = np.array([ii, ii+jj+1])
+		nn = nn+jj+1
+	return uvs, bllengths, label
+
+
+#def simulate_zern(fov = 80, mode=5, livepupilrad=None, mask=None):
+#	"""
+#	Default Astig_1 0.1 radians
+#	"""
+#	# Define our coordinates for wavefront aberration of choice
+#	y,x = np.indices((fov,fov)) - fov/2
+#	if livepupilrad is not None:
+#		rho = np.sqrt( (x*x) + (y*y) ) / livepupilrad
+#	else:
+#		rho = np.sqrt( (x*x) + (y*y) ) / (fov/2) 
+#	theta = np.arctan2(y,x)
+#	if hasattr(mode, "__iter__"):
+#		aberr = np.zeros(rho.shape)
+#		frac = 1/len(mode)
+#		for md in mode:
+#			aberr += zern.zernikel(md,rho,theta)
+#	else:
+#		aberr = zern.zernikel(mode, rho, theta)
+#	if mask is not None:
+#		mask = mask
+#	else:
+#		mask = np.ones((fov,fov))
+#	mask_aberr = mask * aberr
+#	# print out rms of this aberration over circular pupil?  - AS
+#	print np.var(aberr[rho<1])
+#	return mask_aberr, aberr
+#
+#def avg_piston(aberr, positions, mask, R = 20, point=False):
+#	"""
+###	aberr is the full square array aberration
+#
+#	positions are approximaete coordinates of mask holes recorded by eye
+#
+#	mask is the NRM or other pupil mask for over which to compute average phases
+#
+#	R - radius to enclose mask holes around coordinate points (also by eye for now)
+#	    purpose to choose holes one by one. 
+#
+#	point -- should this return average phase values in a single pixel (TRUE)
+#		 or over the whole mask support (FALSE). Recommended to remain FALSE.
+#	"""
+#	fov = aberr.shape[0]
+#	avgphase = np.zeros(aberr.shape) #radians
+#	holemask = avgphase.copy()
+#	x,y = np.indices(holemask.shape)
+#	debug = avgphase.copy()
+#	new_support = avgphase.copy()
+#	for ii,pos in enumerate(positions):
+#		holemask = makedisk(fov, R, ctr = (pos[0] - fov/2, pos[1] - fov/2))
+#		debug+=holemask
+#		holemask[abs(holemask*mask)>0] = 1 
+#		holemask[abs(holemask*mask)==0] = 0
+#		if not point:
+#			avgphase[holemask==1] = aberr[holemask==1].sum() / len(aberr[holemask==1])
+#		else:
+#			#int(np.sum(holemask*x)/holemask.sum()),int(np.sum(holemask*y)/holemask.sum())
+#			avgphase[int(np.sum(holemask*x)/holemask.sum()),\
+#			 int(np.sum(holemask*y)/holemask.sum())] = \
+#					aberr[holemask==1].sum() / len(aberr[holemask==1])
+#		#plt.figure()
+#		#plt.imshow(avgphase)
+#		#plt.figure()
+#		#plt.imshow(debug)
+#		#plt.show()
+#	new_support[abs(avgphase)>0] = 1
+#"""
+#	return avgphase, new_support
--- test_gs.py
+++ test_gs.py
@@ -28,70 +28,253 @@ import numpy as np
 from astropy.io import fits
 import matplotlib.pyplot as plt
 import zernike_AG as zern
+from gsgs import gsalgo
+#from poppy.matrixDFT import matrix_dft as mft
 from matrixDFT import matrix_dft as mft
-from simtools import *
+from NRM_mask_definitions import NRM_mask_definitions
+from simtools import makedisk, mas2rad, baselinify, makeA
 
-nrm_support = fits.open("test_nrm_support.fits")[0].data
-nrm_aberr, aberr = simulate_zern(fov=nrm_support.shape[0], mask = nrm_support)
 
-# provide rough hole positions by eye
-pos_byeye = [[147,255], [179,289], [222, 274], [276,218], [285,174], [220,113], [134,139]]
-avgpist = avg_piston(nrm_aberr, pos_byeye, nrm_support)
+def generate_aberration(npix, nz, pv, livepupilrad=None, debug=False):
+    """
+    Generate a zero mean aberration (w/o piston) in radians
+    """
+    y,x = np.indices((npix,npix)) - npix/2.0
+    if livepupilrad is not None:
+        rho = np.sqrt( (x*x) + (y*y) ) / livepupilrad
+    else:
+        rho = np.sqrt( (x*x) + (y*y) ) / (npix/2.0)
+    theta = np.arctan2(y,x)
 
-fullpupmask = makedisk(nrm_support.shape[0], R = 128, array='EVEN')
-fullpupaberr = fullpupmask*np.exp(1j*aberr)
-npix=400
-fullE = mft(fullpupaberr, npix/2,npix)
-fullpsf = abs(fullE)**2
+    aberr = np.zeros(rho.shape)
+    for z in range(nz):
+        aberr += np.random.rand()*zern.zernikel(z+1,rho,theta)
+        aberr[rho>1] = 0
+        if debug:
+            plt.imshow(aberr)
+            plt.show()
 
-def test_simple_GS(psf, pupilsupport, aberr):
-	""" First test normal gerchberg-saxton without noise to see if things 
-	    are going the right way
-	"""
-	psf = fullpsf.copy()
-	pupil = fullpupmask.copy() # pupil support shape
-	masked_aberr = aberr*fullpupmask
-	plt.figure()
-	plt.title("Pupil Support")
-	plt.imshow(pupilsupport)
-	plt.show()
-	npix = pupil.shape[0]
-	u = npix/2
-	for ii in range(20):
-		a = mft(pupil, u, npix)
-		amp = np.sqrt(psf)
-		pha = np.angle(a)
-		pupil = mft(amp*np.exp(1j*pha), u, npix, inverse=True)*pupilsupport
-		# Just to make sure abs() and np.angle() are working as we expect
-		# When I use the amplitude of pupil support here my GS loop
-		# converges on the wrong answer -- some weird quad-symmetric thing
-		# need to use abs(pupil) even if it's not even illumination
-		# just constrain the support by multiplying at the end
-		pupil = abs(pupil)*np.exp(1j*np.angle(pupil))*pupilsupport
-		#if ii%15 == 0:
-			#plt.figure()
-			#plt.title("simple GS pupil iteration # {0}".format(ii+1))
-			#plt.imshow(np.angle(pupil))
-			#plt.colorbar()
-			#plt.show()
-	plt.figure()
-	plt.title("original aberration")
-	plt.imshow(masked_aberr)
-	plt.colorbar()
-	plt.figure()
-	plt.title("simple GS pupil iteration # {0}".format(ii+1))
-	plt.imshow(np.angle(pupil))
-	plt.colorbar()
-	plt.show()
+    aberr = aberr - np.mean(aberr[rho<=1])
+    # force P2V
+    aberr = pv * aberr/(aberr.max() - aberr.min())
+    print "rms pupil aberr:", np.sqrt(np.var(aberr[rho<1]))
+    pupmask = np.ones(rho.shape)
+    pupmask[rho>1] = 0
+    return aberr, pupmask
 
-def test_constrained_GS():
-	
-	gs = NRM_GS(fullpsf, fullpupmask, avgpist, nrm_support)
-	gs.orig_aberr = aberr
-	gs.find_wavefront()
+def make_PSF(pupmask, aberr, lam_c, telD, pixscale, bandwidth=0, debug=False):
+    npix = pupmask.shape[0]
+    nlamD = npix*pixscale/(lam_c/telD)
+    if bandwidth == 0:
+        pupil = abs(pupmask)*np.exp(1j*aberr)
+        efield = mft(pupil, nlamD, npix)
+        psf = abs(efield)**2
+
+    else:
+        nwavs = 10
+        lambdas = np.linspace(lam_c - (bandwidth/2)*lam_c, lam_c + (bandwidth/2)*lam_c, num=nwavs)
+        psf = np.zeros((npix,npix))
+        for qq, lam in enumerate(lambdas):
+            pupphase = aberr*lam_c/lam # scale the pupil phase by lambda
+            pupil = np.abs(pupmask)*np.exp(1j*pupphase)
+            efield = mft(pupil, nlamD, npix)
+            psf += abs(efield)**2
+
+    if debug:
+        print "Number of pixels in image:", npix
+        print "Number of lam/D in image:", nlamD
+        print "Number of lam/D per pixel:", nlamD/npix
+        plt.set_cmap("gray")
+        plt.subplot(131)
+        plt.title("pupil mask")
+        plt.imshow(pupmask)
+        plt.subplot(132)
+        plt.title("aberration")
+        plt.imshow(aberr)
+        plt.subplot(133)
+        plt.title("PSF")
+        plt.imshow(abs(efield)[100:-100, 100:-100])
+        plt.show()
+
+    return psf
+
+def make_mask(maskobj, npix, OD, debug=False):
+    """
+    OD will be set to half the array size to match defaults above
+    """
+    m2pix = (npix)/OD
+    pupil = np.zeros((npix, npix))
+    for holectr in maskobj.ctrs:
+        #center = (0.5*npix + holectr[0]*m2pix -0.5, 0.5*npix + holectr[1]*m2pix -0.5)
+        center = (holectr[0]*m2pix, holectr[1]*m2pix)
+        pupil += makedisk(npix, R = 0.5*maskobj.hdia*m2pix, ctr=center)
+
+    if debug:
+       plt.title("making pupil mask")
+       plt.imshow(pupil, cmap="gray")
+       plt.show()
+
+    return pupil
+
+def measure_nrm_pistons(maskobj, nrmpsf, telD, debug=False):
+
+    baselines, lengths, labels = baselinify(maskobj.ctrs)
+    scalefac = 0.48*nrmpsf.shape[0]/telD
+
+    # calculate complex visibilities
+    cvarray = mft(nrmpsf, nrmpsf.shape[0], nrmpsf.shape[0])
+    cvpha = np.angle(cvarray)
+
+    fringephases = np.zeros(len(lengths))
+    phaseloc = np.zeros((cvpha.shape[0], cvpha.shape[1]))
+    for bl in range(len(lengths)):
+        ctr = baselines[bl, 0]*scalefac, baselines[bl, 1]*scalefac
+        # mask a small region
+        region = makedisk(cvpha.shape[0], R=5, ctr=ctr)
+
+        fringephases[bl] = np.mean(cvpha[region==1])
+        phaseloc[region==1] = fringephases[bl]
+
+    bl_mat = makeA(len(maskobj.ctrs))
+    hole_phases = - np.dot(np.linalg.pinv(bl_mat), fringephases)
+
+    if debug:
+        plt.set_cmap("gray")
+        plt.subplot(1,4,1)
+        plt.title("visibility amp array")
+        plt.imshow(np.sqrt(abs(cvarray)))
+        plt.subplot(1,4,2)
+        plt.title("visibility phase array")
+        threshold = np.ones(cvpha.shape)
+        threshold[abs(cvarray)<0.01] = 0
+        plt.imshow(cvpha*threshold)
+        plt.subplot(1,4,3)
+        plt.title("selection")
+        plt.imshow(phaseloc)
+        plt.subplot(1,4,4)
+        plt.title("PSF")
+        plt.imshow(nrmpsf[100:-100, 100:-100])
+        plt.show()
+
+    print "Hole phases measured:", hole_phases
+    return hole_phases
+
+def create_pupilestim_array(maskobj, pistons, npix, OD, debug=False):
+
+    m2pix = 0.95*(npix)/OD
+    pupilest = np.zeros((npix, npix))
+    for q, ctrloc in enumerate(maskobj.ctrs):
+        #ctrs = cvpha.shape[0]/2. - ctrloc[0], cvpha.shape[0]/2. - ctrloc[1]
+        ctrs = ctrloc[0]*m2pix, ctrloc[1]*m2pix
+        mask = makedisk(npix, 8, ctr=ctrs)
+        #print q
+        #print pistons
+        pupilest[mask==1] = pistons[q]
+
+    if debug:
+        plt.title("pupil estimate")
+        plt.imshow(pupilest)
+        plt.show()
+
+    return pupilest
+
+def run_gsgs(psf, pupsupport, pupconstraint, D, lam, pscale):
+
+    npix = pupsupport.shape[0]
+    lamD_pix = pscale / (lam/D) # lam/D per pix
+    nlamD = lamD_pix*npix
+    
+    print "npix:", npix, "gives", nlamD, "lam/D across the image"
+
+    nrm_support = pupconstraint.copy()
+    nrm_support[abs(pupconstraint)>0] = 1
+    gs = gsalgo.NRM_GS(psf, pupsupport, pupconstraint, nrm_support)
+    gs.nlamD = nlamD
+    gs.damping = True
+    gs.zsmooth=True
+    gs.condition_c = 1.0e-2
+    gs.nitermax = 500
+    gs.nitermax_c = 500
+    wavefront = gs.find_wavefront()
+
+    print "==========================="
+    print wavefront
+    print np.nanmax(wavefront)
+    print wavefront.imag
+    print wavefront.real
+    print "==========================="
+
+    #np.savetxt(save.replace(".fits", "_convlist.txt"), gs.metriclist)
+    #fits.PrimaryHDU(data=np.angle(wavefront), header=psfhdr).writeto(save, clobber=True)
+
+    plt.set_cmap("BrBG")
+    plt.subplot(121)
+    plt.imshow(np.angle(wavefront))
+    plt.axis("off")
+    plt.colorbar()
+    plt.subplot(122)
+    plt.semilogy(np.arange(len(gs.metriclist)), gs.metriclist)
+    plt.show()
+    return wavefront, gs.pup_i
+
+def simple_demo():
+
+    # set up pupil/image params
+    telD = 6.5
+    lam1 = 4.3e-6
+
+    aberr, pupsupport = generate_aberration(npix=256, nz=5, pv=2.0)
+
+    psf_430 = make_PSF(pupsupport, aberr, 4.3e-6, 6.5, mas2rad(65), bandwidth=0.05)
+
+    maskdef = NRM_mask_definitions(maskname="jwst_g7s6c")
+    maskdef.ctrs = np.array(maskdef.ctrs)
+
+    nrmask = make_mask(maskdef, aberr.shape[0], telD)
+    nrm_430 = make_PSF(nrmask, aberr, 4.3e-6, 6.5, mas2rad(65), bandwidth=0.05)
+
+    pistons_430 = measure_nrm_pistons(maskdef, nrm_430,telD, debug=True)
+    pupestim = create_pupilestim_array(maskdef, pistons_430, aberr.shape[0], telD)
+
+    recovered, pup_i = run_gsgs(psf_430, pupsupport, pupestim, telD, lam1, mas2rad(65))
+
+    print "************ TEST # 1: Does it work at all? *************"
+    print "How did we do?", "rms error:", np.std(aberr - np.angle(recovered))
+    vmax = aberr.max()
+    vmin = aberr.min()
+    plt.subplot(231)
+    plt.title("Starting aberration")
+    plt.imshow(aberr, vmax=vmax, vmin=vmin)
+    plt.axis("off")
+    plt.colorbar()
+    plt.subplot(234)
+    plt.title("Recovered aberration")
+    plt.imshow(np.angle(recovered), vmax=vmax, vmin=vmin)
+    plt.axis("off")
+    plt.colorbar()
+    plt.subplot(233)
+    plt.title("Constraint")
+    plt.imshow(pupestim, vmin=vmin, vmax=vmax)
+    plt.axis("off")
+    plt.colorbar()
+    plt.subplot(232)
+    plt.title("NRM")
+    plt.imshow(nrmask)
+    plt.axis("off")
+    plt.colorbar()
+    plt.subplot(236)
+    plt.title("mask * aberration")
+    plt.imshow(nrmask*aberr, vmax=vmax, vmin=vmin)
+    plt.axis("off")
+    plt.colorbar()
+    plt.subplot(235)
+    plt.title("residual aberration")
+    plt.imshow(aberr - np.angle(recovered))
+    plt.axis("off")
+    plt.colorbar()
+    plt.show()
 
 if __name__ == "__main__":
 
-	test_simple_GS()
-	test_constrained_GS()
+    simple_demo()
 
